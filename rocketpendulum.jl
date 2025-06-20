bd6cd8ce798413183b59302b6dc77dc35b6a120f
Matthew Galarza <mgalarza.rpi@gmail.com>
2025-06-20 09:40:35 -0400

    Update rocketpendulum.jl

--- rocketpendulum.jl
+++ rocketpendulum.jl
@@ -504,6 +504,8 @@ record(fig, "pendulum.mp4", times; framerate=60)
 
 
 
+
+
 using GLMakie, Observables, DifferentialEquations, LinearAlgebra, Sundials, OrdinaryDiffEq, StaticArrays
 
 # ----------------------------- System Parameters -----------------------------
